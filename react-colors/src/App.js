40f81a48571a30d84d891c0ab9cbb1687ad4ac47
Anonymous <worldtraveler757@gmail.com>
2020-08-13 15:16:59 +0200

    add color slider

--- react-colors/src/App.js
+++ react-colors/src/App.js
@@ -4,10 +4,9 @@ import seedPalettes from './seedPalettes'
 import { generatePalette } from './helpers/ColorHelpers';
 
 function App() {
-  console.log(generatePalette(seedPalettes[4]))
   return (
     <div className="App">
-      <Palette { ...seedPalettes[4] } />
+      <Palette palette={generatePalette(seedPalettes[4])} />
     </div>
   );
 }
--- react-colors/src/components/Palette.js
+++ react-colors/src/components/Palette.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react'
-import ColorBox from './ColorBox'
-import './Palette.css'
-
+import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import './Palette.css';
 
 export default class Palette extends Component {
-    render() {
-        const colorBoxes = this.props.colors.map(color => (
-            <ColorBox background={color.color} name={color.name} />
-        ));
-        return (
-            <div className="Palette">
-                {/* Navbar goes here */}
-                <div className="Palette-colors">
-                    {colorBoxes}
-                </div>
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props);
+		this.state = { level: 500 };
+		this.changeLevel = this.changeLevel.bind(this);
+	}
+
+	changeLevel(level) {
+		this.setState({ level });
+	}
+
+	render() {
+		const { colors } = this.props.palette;
+		const { level } = this.state;
+		const colorBoxes = colors[level].map((color) => <ColorBox background={color.hex} name={color.name} />);
+		return (
+			<div className="Palette">
+				<div className="slider">
+					<Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={this.changeLevel} />
+				</div>
+				{/* Navbar goes here */}
+				<div className="Palette-colors">{colorBoxes}</div>
+			</div>
+		);
+	}
 }
