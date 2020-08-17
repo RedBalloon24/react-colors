7d3b0302265214e5512baadfda02dd02715954df
Anonymous <worldtraveler757@gmail.com>
2020-08-17 12:17:03 +0200

    add mini palette delete

--- react-colors/src/App.js
+++ react-colors/src/App.js
@@ -16,12 +16,22 @@ class App extends Component {
 		};
 		this.savePalette = this.savePalette.bind(this);
 		this.findPalette = this.findPalette.bind(this);
+		this.deletePalette = this.deletePalette.bind(this);
 	}
 
 	findPalette(id) {
 		return this.state.palettes.find((palette) => {
 			return palette.id === id;
 		});
+	}
+
+	deletePalette(id) {
+		this.setState(
+			(st) => ({
+				palettes: st.palettes.filter((palette) => palette.id !== id)
+			}),
+			this.syncLocalStorage
+		);
 	}
 
 	savePalette(newPalette) {
@@ -51,7 +61,13 @@ class App extends Component {
 				<Route
 					exact
 					path="/"
-					render={(routeProps) => <PaletteList palettes={this.state.palettes} {...routeProps} />}
+					render={(routeProps) => (
+						<PaletteList
+							palettes={this.state.palettes}
+							deletePalette={this.deletePalette}
+							{...routeProps}
+						/>
+					)}
 				/>
 				<Route
 					exact
